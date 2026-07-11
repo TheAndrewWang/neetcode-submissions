d9d3d6c81a7e715fd751b30cda225b377009e6a8
Andrew Wang <andrewwangpa@gmail.com>
2026-07-11 14:21:11 -0400

    Add: subsets - submission-1

---
+++ Data Structures & Algorithms/subsets/submission-1.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    vector<vector<int>> subsets(vector<int>& nums) {
+        vector<vector<int>> res;
+        vector<int> subset;
+        backtrack(nums, 0, subset, res);
+        return res;
+    }
+
+private:
+    void backtrack(const vector<int>& nums, int i, vector<int>& subset, vector<vector<int>>& res) {
+        if (i >= nums.size()) {
+            res.push_back(subset);
+            return;
+        }
+
+        subset.push_back(nums[i]);
+        backtrack(nums, i + 1, subset, res);
+        subset.pop_back();
+        backtrack(nums, i + 1, subset, res);
+
+        return;
+    }
+};
